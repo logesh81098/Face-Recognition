8069ff85a318b113fa054260cc4a6396f797af48
Logesh Shanmugavel <logesh81098@gmail.com>
2025-01-07 19:08:22 +0530

    Deployed IAM role and Policy for Lambda function to collect faceprints from Rekognition and store it in DynamoDB

--- module/iam-role/main.tf
+++ module/iam-role/main.tf
@@ -69,3 +69,94 @@ resource "aws_iam_role_policy_attachment" "lambda-to-create-collection-id" {
   role = aws_iam_role.iam-role-lambda-to-create-collection-id.id
   policy_arn = aws_iam_policy.iam-policy-lambda-to-create-collection-id.arn
 }
+
+
+#############################################################################################################################
+#############################################################################################################################
+
+##############################################################################################################################
+#                                          Deploying IAM role 
+##############################################################################################################################
+
+###### Creating IAM Role for Lambda function to collect faceprints and store it in DynamoDB
+
+resource "aws_iam_role" "iam-role-putitems-in-dynamodb" {
+  name = "lambda-to-store-faceprints-dynamodb"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+    "Effect": "Allow",
+    "Principal": {
+      "Service": "lambda.amazonaws.com"
+    },
+    "Action": "sts:AssumeRole"
+  }
+  ]
+}  
+EOF
+}
+
+##############################################################################################################################
+#                                          Deploying IAM Policy
+##############################################################################################################################
+
+#IAM Policy for Lambda to collect faceprints and store it in DynamoDB
+
+resource "aws_iam_policy" "iam-policy-putitems-in-dynamodb" {
+  name = "lambda-to-store-faceprints-dynamodb"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+  "Sid": "CloudWatchLoggroup",
+  "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+            ],
+      "Resource": "arn:aws:logs:*:*:*"
+  },
+  {
+      "Sid": "Rekognitionindexface",
+      "Effect": "Allow",
+      "Action": [
+        "rekognition:IndexFaces"
+      ],
+      "Resource": "arn:aws:rekognition:*:*:collection/*"
+    },
+    {
+      "Sid": "DynamoDBputfaceprints",
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:PutItem"
+      ],
+      "Resource": "arn:aws:dynamodb:*:*:table/face-prints-table"
+    },
+    {
+      "Sid": "S3fetchimages",
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:HeadObject"
+      ],
+      "Resource": "arn:aws:s3:::face-rekognition-source-bucket-logesh/*"
+    }
+  ]
+}  
+EOF
+}
+
+##############################################################################################################################
+#                                          Role Policy Attachment
+##############################################################################################################################
+
+#Role and Policy attachment for lambda to store faceprint in DynamoDB
+
+resource "aws_iam_role_policy_attachment" "dynamodb-role-policy-attachment" {
+  role = aws_iam_role.iam-role-putitems-in-dynamodb.id
+  policy_arn = aws_iam_policy.iam-policy-putitems-in-dynamodb.arn
+}
