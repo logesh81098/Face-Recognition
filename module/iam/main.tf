c54f62c5246b85ed5da7bf5a6152c2d24dfe127c
Logesh Shanmugavel <logesh81098@gmail.com>
2025-08-12 13:42:14 +0530

    Deployed IAM Role and Policy for Lambda Function to create Rekognition CollectionID

---
+++ module/iam/main.tf
@@ -0,0 +1,80 @@
+#############################################################################################################################################################################
+#                                                                      IAM Role
+#############################################################################################################################################################################
+
+#IAM Role for Lambda Function to create CollectionID in AWS Rekognition service
+
+resource "aws_iam_role" "collectionid-role" {
+  name = "Rekognition-CollectionID-Role"
+  description = "IAM Role for Lambda Function to create CollectionID in AWS Rekognition service"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            }
+        }
+    ]
+}
+EOF
+  tags = {
+    Name = "Rekognition-CollectionID-Role"
+    Project = "Recognizing-faces-using-AWS-Rekognition-service"
+  }
+}
+
+
+#############################################################################################################################################################################
+#                                                                      IAM Policy
+#############################################################################################################################################################################
+
+#IAM Policy for Lambda Function to create CollectionID in AWS Rekognition service
+
+resource "aws_iam_policy" "collectionid-policy" {
+  name = "Rekognition-CollectionID-Policy"
+  description = "IAM Policy for Lambda Function to create CollectionID in AWS Rekognition service"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "CloudWatchLogGroup",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:*:*:*"
+        },
+        {
+            "Sid": "RekognitionCollectionID",
+            "Effect": "Allow",
+            "Action": [
+                "rekognition:CreateCollection",
+                "rekognition:DeleteCollection",
+                "rekognition:ListCollections"
+            ],
+            "Resource": "*"
+        }
+    ]
+}  
+EOF
+  tags = {
+    Name = "Rekognition-CollectionID-Policy"
+    Project = "Recognizing-faces-using-AWS-Rekognition-service"
+  }
+}
+
+#############################################################################################################################################################################
+#                                                                  Role & Policy Attachment
+#############################################################################################################################################################################
+
+resource "aws_iam_role_policy_attachment" "collectionid" {
+  role = aws_iam_role.collectionid-role.id
+  policy_arn = aws_iam_policy.collectionid-policy.arn
+}
