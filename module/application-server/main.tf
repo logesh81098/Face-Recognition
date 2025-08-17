a4d803fb703eb8b54938cd5369f1f423e31be8fd
Logesh Shanmugavel <logesh81098@gmail.com>
2025-08-17 17:20:40 +0530

    Added user data for Application server to install git, docker, jenkins, python

--- module/application-server/main.tf
+++ module/application-server/main.tf
@@ -18,4 +18,35 @@ resource "aws_instance" "application-server" {
     Name = "Rekognition-Application-Server"
     Project = "Recognizing-faces-using-AWS-Rekognition-service"
   }
+  user_data = <<-EOF
+  #!bin/bash
+  sudo su
+  set -eux
+  dnf update -y
+  dnf upgrade -y
+  dnf install git -y
+  git --version
+  dnf install docker -y
+  systemctl enable docker
+  systemctl start docker
+  sleep 10
+  systemctl status docker
+  usermod -aG docker ec2-user
+  dnf install -y python3 python3-pip 
+  pip install boto3
+  cd /
+  git clone https://github.com/logesh81098/Face-Recognition.git
+  cd Face-Recognition/
+  python3 upload-images-to-s3.py
+  sleep 10
+  sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+  sudo dnf install java-17-amazon-corretto -y
+  sudo dnf install jenkins -y
+  sudo systemctl enable jenkins
+  sudo systemctl start jenkins
+  sudo systemctl status jenkins
+  usermod -aG docker jenkins
+  reboot
+  EOF
 }
